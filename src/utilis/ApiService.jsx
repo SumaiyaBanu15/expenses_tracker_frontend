bfcbcc2d6cdd84aa3899730d0dd2b1e3fc8b6f94
SumaiyaBanu15 <sumaiyanishar115@gmail.com>
2024-08-19 10:03:20 +0530

    url updated

--- src/utilis/ApiService.jsx
+++ src/utilis/ApiService.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // const baseUrl = 
 const AxiosService = axios.create({
-    baseURL: `${import.meta.env.VITE_API_URL}`,
-    // baseURL: 'http://localhost:8000',
+    // baseURL: `${import.meta.env.VITE_API_URL}`,
+    baseURL: `${import.meta.env.VITE_API_URL}` || 'http://localhost:8000',
     headers: {
         'Content-Type' : "application/json",
     }
